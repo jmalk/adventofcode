adbd1eec724e43f11f8e2830b98e312c5a14691e
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-10 16:43:51 +0000

    Add more specs and mapreduce implementation

--- spec/1/NotQuiteLispSpec.js
+++ spec/1/NotQuiteLispSpec.js
@@ -5,8 +5,35 @@ describe('Interpretation of instrutions given to Santa', function() {
 
         var instructions = '';
         var floorSantaIsOn = whatFloorIsSantaOn(instructions);
-        
+
         expect(floorSantaIsOn).toBe(0);
+
+    });
+
+    it('Should tell us Santa is on the ground floor if the instructions (()) are given', function() {
+
+        var instructions = '(())';
+        var floorSantaIsOn = whatFloorIsSantaOn(instructions);
+
+        expect(floorSantaIsOn).toBe(0);
+
+    });
+
+    it('Should tell us Santa is on the ground floor if the instructions ()() are given', function() {
+
+        var instructions = '()()';
+        var floorSantaIsOn = whatFloorIsSantaOn(instructions);
+
+        expect(floorSantaIsOn).toBe(0);
+
+    });
+
+    it('Should tell us Santa is on the third floor if the instructions ((( are given', function() {
+
+        var instructions = '(((';
+        var floorSantaIsOn = whatFloorIsSantaOn(instructions);
+
+        expect(floorSantaIsOn).toBe(3);
 
     });
 
--- src/1/NotQuiteLisp.js
+++ src/1/NotQuiteLisp.js
@@ -1,4 +1,19 @@
 function whatFloorIsSantaOn(instructions) {
     'use strict';
-    return 0;
+
+    var instructionsArray = instructions.split('');
+
+    var sumOfInstructions = instructionsArray.map(function(character) {
+    if (character === '(') {
+        return 1;
+    } else if (character === ')') {
+        return -1;
+    } else {
+        return 0;
+    }
+    }).reduce(function(prev, next) {
+        return prev + next;
+    }, 0);
+
+    return sumOfInstructions;
 }
