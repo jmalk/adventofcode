532b812b6944049016e7668aac569a7c9ba2b0f9
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-10 11:04:50 +0000

    Add Jasmine spec runner and failing test

---
+++ spec/1/NotQuiteLispSpec.js
@@ -0,0 +1,10 @@
+describe("Interpretation of instrutions given to Santa", function() {
+    "use strict";
+
+    it("Should tell us Santa is on the ground floor if santa has no instructions", function() {
+        "use strict";
+        expect(true).toBe(false);
+    });
+
+});
+
