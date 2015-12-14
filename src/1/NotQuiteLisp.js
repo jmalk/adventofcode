caa73bbbea72aba242e8bf1dfab196ff09aacf0e
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-14 13:38:46 +0000

    Autoindent

--- src/1/NotQuiteLisp.js
+++ src/1/NotQuiteLisp.js
@@ -6,13 +6,13 @@ function whatFloorIsSantaOn(instructions) {
     var instructionsArray = instructions.split('');
 
     var sumOfInstructions = instructionsArray.map(function(character) {
-    if (character === '(') {
-        return 1;
-    } else if (character === ')') {
-        return -1;
-    } else {
-        return 0;
-    }
+        if (character === '(') {
+            return 1;
+        } else if (character === ')') {
+            return -1;
+        } else {
+            return 0;
+        }
     }).reduce(function(prev, next) {
         return prev + next;
     }, 0);
