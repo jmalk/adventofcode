f8187a15b25477bc5cba5d21c512fd2885e563ee
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-15 16:18:38 +0000

    Working implementation

--- 2/IWasToldThereWouldBeNoMath.js
+++ 2/IWasToldThereWouldBeNoMath.js
@@ -1,9 +1,48 @@
+'use strict';
 // Read the data from the file
+var fs = require('fs');
+
+var presentDimensions = fs.readFileSync('presentDimensions', 'utf8', function(error, text) {
+    if (error) {
+        throw error;
+    }
+    return text;
+});
 
 // Divide the data into array, each element is one present
+var arrayOfPresentDimensions = presentDimensions.split('\n');
 
 // Divide each element into array of its three dimensions
+function splitOnx (string) {
+    return string.split('x');
+}
+
+var matrixOfPresentDimensions = arrayOfPresentDimensions.map(splitOnx);
+console.log(matrixOfPresentDimensions);
+
+// 2*l*w + 2*w*h + 2*h*l
+function surfaceAreaOfBox(arrayOfDimensions) {
+    var box = arrayOfDimensions;
+    return 2 * ( box[0] * box[1] + box[1] * box[2] + box[2] * box[0] );
+}
+
+// PLUS shortest side * second shortest side
+function areaOfSmallestSide(arrayOfDimensions) {
+    var box = arrayOfDimensions;
+    var smallestSide = box.sort(function(a, b) { return a - b; })[0];
+    console.log(smallestSide);
+    var secondSmallestSide = box.sort(function(a, b) { return a - b; })[1];
+    console.log(secondSmallestSide);
+    return smallestSide * secondSmallestSide;
+}
 
 // For each present:
-// 2*l*w + 2*w*h + 2*h*l
-// PLUS shortest side * second shortest side
+function squareFeetForPresent(arrayOfDimensions) {
+    var box = arrayOfDimensions;
+    return surfaceAreaOfBox(box) + areaOfSmallestSide(box);
+}
+
+var paperRequired = matrixOfPresentDimensions.map(squareFeetForPresent)
+                                                 .reduce(function (a, b) { return a + b; });
+
+console.log(paperRequired);
