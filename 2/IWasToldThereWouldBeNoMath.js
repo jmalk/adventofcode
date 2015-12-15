666625a45b6730e7992658eb1a1aa12d1a1d2f34
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-15 11:51:35 +0000

    Comments to spell out problem

---
+++ 2/IWasToldThereWouldBeNoMath.js
@@ -0,0 +1,9 @@
+// Read the data from the file
+
+// Divide the data into array, each element is one present
+
+// Divide each element into array of its three dimensions
+
+// For each present:
+// 2*l*w + 2*w*h + 2*h*l
+// PLUS shortest side * second shortest side
