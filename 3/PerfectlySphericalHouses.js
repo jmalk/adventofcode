faff680b9081f7eeb14a53fb3d582548bb365391
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-16 17:17:19 +0000

    Add object of directions, state solution

--- 3/PerfectlySphericalHouses.js
+++ 3/PerfectlySphericalHouses.js
@@ -10,3 +10,14 @@ var instructions = fs.readFileSync('directions.txt', 'utf8', function(error, tex
 });
 
 console.log(instructions);
+
+var cardinalDirections = {
+    "^": [ 0,  1],
+    ">": [ 1,  0],
+    "v": [ 0, -1],
+    "<": [-1,  0]
+};
+
+var uniqueHousesVisited = [];
+
+console.log('At least one present was delivered to', uniqueHousesVisited.length, 'different houses.');
