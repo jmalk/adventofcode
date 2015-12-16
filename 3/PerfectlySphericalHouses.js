6bae2225651d4f0fa60847340dba9a9541a7b737
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-16 17:21:02 +0000

    Add hacky way of updating position

--- 3/PerfectlySphericalHouses.js
+++ 3/PerfectlySphericalHouses.js
@@ -11,12 +11,21 @@ var instructions = fs.readFileSync('directions.txt', 'utf8', function(error, tex
 
 console.log(instructions);
 
+var currentPosition = [0, 0];
+
+var testInstructions = "^";
+
 var cardinalDirections = {
     "^": [ 0,  1],
     ">": [ 1,  0],
     "v": [ 0, -1],
     "<": [-1,  0]
 };
+
+console.log(currentPosition);
+currentPosition[0] = currentPosition[0] + cardinalDirections[testInstructions][0];
+currentPosition[1] = currentPosition[1] + cardinalDirections[testInstructions][1];
+console.log(currentPosition);
 
 var uniqueHousesVisited = [];
 
