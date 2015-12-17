5c8009a65ede05858a8c67b1ef3cdd2b8495f434
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-17 15:47:20 +0000

    Working implementation

--- 3/PerfectlySphericalHouses.js
+++ 3/PerfectlySphericalHouses.js
@@ -9,13 +9,12 @@ var instructions = fs.readFileSync('directions.txt', 'utf8', function(error, tex
     return text;
 });
 
-console.log(instructions);
-
 var arrayOfInstructions = instructions.split('');
 
-var currentPosition = [0, 0];
+var startingPosition = [0, 0];
 
-var testInstructions = arrayOfInstructions[0];
+var housesVisited = [startingPosition];
+var uniqueHousesVisited = 1;
 
 var cardinalDirections = {
     "^": [ 0,  1],
@@ -24,11 +23,40 @@ var cardinalDirections = {
     "<": [-1,  0]
 };
 
-console.log(currentPosition);
-currentPosition[0] = currentPosition[0] + cardinalDirections[testInstructions][0];
-currentPosition[1] = currentPosition[1] + cardinalDirections[testInstructions][1];
-console.log(currentPosition);
+function areEqual(arrA, arrB) {
+    return arrA[0] === arrB[0] && arrA[1] === arrB[1];
+}
 
-var housesVisited = [];
+function arrayContainsVector(arr, vec) {
+    var contains = false;
 
-console.log('At least one present was delivered to', housesVisited.length, 'different houses.');
+    for (var i = 0; i < arr.length; i++) {
+        if (areEqual(arr[i], vec)) {
+            contains = true;
+        }
+    }
+
+    return contains;
+}
+
+arrayOfInstructions.forEach(updateHousesVisitedAndUnique);
+
+function updateHousesVisitedAndUnique(direction) {
+    var newLocation = [];
+    newLocation[0] = housesVisited[housesVisited.length - 1][0] +
+                     cardinalDirections[direction][0];
+    newLocation[1] = housesVisited[housesVisited.length - 1][1] +
+                     cardinalDirections[direction][1];
+
+    if (!arrayContainsVector(housesVisited, newLocation)) {
+        uniqueHousesVisited += 1;
+    }
+
+    housesVisited.push(newLocation);
+}
+
+console.log(housesVisited);
+
+console.log('', housesVisited.length, 'presents were delivered.');
+
+console.log('At least one present was delivered to', uniqueHousesVisited, 'different houses.');
