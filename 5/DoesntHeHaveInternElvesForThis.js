297f6263d7ddb693b02e77fcf0605e1d9454e5fc
Josh Malkinson <josh.malkinson@gmail.com>
2015-12-18 22:36:01 +0000

    Working implementation

--- 5/DoesntHeHaveInternElvesForThis.js
+++ 5/DoesntHeHaveInternElvesForThis.js
@@ -1,3 +1,4 @@
+'use strict';
 var fs = require('fs');
 
 var strings = fs.readFileSync('strings.txt', 'utf8', function(error, text) {
@@ -7,4 +8,42 @@ var strings = fs.readFileSync('strings.txt', 'utf8', function(error, text) {
     return text;
 });
 
-console.log(strings);
+strings = strings.split('\n');
+
+function isNice(string) {
+    return containsThreeVowels(string) &&
+           containsDoubleLetter(string) &&
+           !containsBannedSubstring(string);
+}
+
+function containsThreeVowels(string) {
+    return /(.*(a|e|i|o|u).*){3,}/.test(string);
+}
+
+function containsDoubleLetter(string) {
+    var characters = string.split('');
+
+    for (var i = 1; i < characters.length; i++) {
+        var charIsLetter = /[A-Za-z]/.test(characters[i]);
+        var charSameAsPrevious = (characters[i] === characters[i - 1]);
+        if (charIsLetter && charSameAsPrevious) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function containsBannedSubstring(string) {
+    return /(ab|cd|pq|xy)/.test(string);
+}
+
+var numberOfNiceStrings = 0;
+
+strings.forEach(function(string) {
+    if (isNice(string)) {
+        numberOfNiceStrings +=1;
+    }
+});
+
+console.log('There were', numberOfNiceStrings, 'nice strings.');
