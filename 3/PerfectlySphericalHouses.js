29f150fa3e6fb6e77ed6f45ac7b601a4d846929c
Josh Malkinson <josh.malkinson@gmail.com>
2016-01-26 16:38:40 +0000

    WIP on part 2

--- 3/PerfectlySphericalHouses.js
+++ 3/PerfectlySphericalHouses.js
@@ -60,3 +60,40 @@ console.log(housesVisited);
 console.log('', housesVisited.length, 'presents were delivered.');
 
 console.log('At least one present was delivered to', uniqueHousesVisited, 'different houses.');
+
+/*** Part two ***/
+
+var santaVisited = [[0, 0]];
+var robotVisited = [[0, 0]];
+
+var housesGotPresents = 1;
+
+function destination(direction, listOfHouses) {
+    var newLocation = [];
+    newLocation[0] = listOfHouses[listOfHouses.length - 1][0] +
+                     cardinalDirections[direction][0];
+    newLocation[1] = listOfHouses[listOfHouses.length - 1][1] +
+                     cardinalDirections[direction][1];
+    return newLocation;
+}
+
+function hasBeenToHouseBefore(housesVisited, house) {
+    return arrayContainsVector(housesVisited, house);
+}
+
+function move(direction, character) {
+    character.push(destination(direction, character));
+    if (!hasBeenToHouseBefore(character, destination(direction, character))) {
+        housesGotPresents += 1;
+    }
+}
+
+arrayOfInstructions.forEach(function(element, index) {
+    if (index % 2 === 0) {
+        move(element, santaVisited, housesGotPresents);
+    } else {
+        move(element, santaVisited, housesGotPresents);
+    }
+});
+
+console.log('With two santas on the case,', housesGotPresents, 'got presents.');
