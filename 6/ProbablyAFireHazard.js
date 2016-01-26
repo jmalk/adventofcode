99f65b4af5301b32a4a49d2563310590a03d42a6
Josh Malkinson <josh.malkinson@gmail.com>
2016-01-26 16:17:37 +0000

    Add a minimal light class

--- 6/ProbablyAFireHazard.js
+++ 6/ProbablyAFireHazard.js
@@ -8,3 +8,18 @@ var v = new Vector(3, 5);
 
 console.log('Expect x to be 3, actually it is', v.x);
 console.log('Expect y to be 5, actually it is', v.y);
+
+function Light(vector, isOn) {
+    'use strict';
+    this.position = vector;
+    this.on = isOn;
+}
+
+Light.prototype.print = function() {
+    'use strict';
+    console.log('x: ' + this.position.x + ', y: ' + this.position.y + ', on: ' + this.isOn);
+};
+
+var testLight = new Light(v, false);
+
+testLight.print();
